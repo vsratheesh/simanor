b65b7ae5e0a6b525148910043b4b63a80d3484cf
vsratheesh <vsratheesh95@gmail.com>
2017-09-25 13:09:03 +0530

    initial setup

---
+++ contact-us.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>.:: Simanor ::.</title>
+    <link rel="icon" href="assets/images/favicon.ico" type='image/x-icon'/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <script src="assets/js/wow.min.js"></script>
+    <script>
+      new WOW().init();
+    </script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+  </head>
+  <body>
+    <div class="navbar-fixed-top desktop_header">
+      <div class="topmost_div">
+        <div class="container">
+          <ul class="nav navbar-nav">
+            <li><a href="#" class="top_div_links" style="border-left: 1px solid #607588"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="franchisees.html" class="top_div_links"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="career.html" class="top_div_links"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="top_div_links"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="top_div_links" style="border-left: 1px solid #607588"><i class="fa fa-phone" aria-hidden="true"></i> +1 646 340 2040</a></li>
+            <li><a href="franchisees.html" class="top_div_links"><i class="fa fa-envelope" aria-hidden="true"></i> info@simanor.com</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="middle_header">
+        <div class="container" style="padding-top: 15px;">
+          <div class="col-sm-3">
+            <a class="navbar-brand" href="index.html" style="padding-top: 7px">
+            <img src="assets/images/logo.png" class="img-responsive" style="width: 175px">
+            </a>
+          </div>
+          <div class="col-sm-9">
+            <ul class="nav navbar-nav navbar-right" style="padding-top: 18px">
+              <li><a href="index.html" class="middle_nav_links"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+              <li><a href="about-us.html" class="middle_nav_links">About Us</a></li>
+              <li><a href="services.html" class="middle_nav_links">Services</a></li>
+              <li><a href="our-team.html" class="middle_nav_links">Our Team</a></li>
+              <li class="dropdown"><a href="#" class="dropdown-toggle middle_nav_links" data-toggle="dropdown">Experience</a>
+                <ul class="dropdown-menu">
+                  <li><a href="experience.html">Completed Transition</a></li>
+                   
+                  <li><a href="current-transaction.html">Current Transaction</a></li>
+                </ul>
+              </li>
+             
+              <li><a href="news.html" class="middle_nav_links">News & Resources</a></li>
+               <li  class="active"><a href="contact-us.php" class="middle_nav_links" style="border: none;">Contact Us</a></li>
+             
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mobile_header">
+      <div class="col-sm-3 col-xs-3">
+        <a href="index.html">
+          <img src="assets/images/logo.png" style="width: 170px">
+        </a>
+      </div>
+      <div class="col-sm-9 col-xs-9">
+        <a href="#" onclick="openNav()">
+        <img src="assets/images/toggle.png" class="pull-right">
+        </a>
+      </div>
+    </div>
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="index.html">Home</a>
+      <a href="about-us.html">About Us</a>
+      <a href="services.html">Services</a>
+      <a href="our-team.html">Our Team</a>
+      <a href="#" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">Experience <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+       <div class="collapse" id="toggleDemo" style="height: 0px;padding-left: 60px;">
+            <ul class="nav nav-list">
+              <li><a href="experience.html" class="sidebar_link">Complete Transaction</a></li>
+              <li><a href="current-transaction.html" class="sidebar_link">Current Transaction</a></li>
+            </ul>
+          </div>
+          <a href="news.html">New & Resouces</a>
+      <a href="contact-us.php">Contact Us</a>
+    </div>
+
+<div class="services_banner_cont top_div">
+      <h1 class="main_headings2">Contact Us</h1>
+      <div class="seprator_3"></div>
+    </div>
+
+<div class="container top_div2">
+        <h4>We are ready when you are.<strong>Let's talk...</strong></h4>
+  <p class="text-justify">Let’s work together to change the way people everywhere bank, shop, eat and travel. Whether you’re looking for faster service, smarter technologies or better ways to keep your customers coming back, we’re ready to help. <strong>Just fill out the form to get the conversation started.</strong></p>
+  <br>
+
+  <div class="col-sm-4">
+      <div>
+      <h1 class="about_heading3">UNITED STATE</h1>
+      <div class="seprator_1"></div>
+            <h4 style="margin-top: 0">New York</h4>
+      <address style="color: #000;padding-left: 0"><i class="fa fa-map-signs" aria-hidden="true" style="color: #fe5722"></i> 1501 Broadway 
+12th Floor
+New York 
+NY 10036</address>
+        <h5 style="line-height: 20px"><i class="fa fa-phone" aria-hidden="true" style="color: #fe5722"></i>  +1 646 340 2040</h5>
+        <h5><i class="fa fa-fax" aria-hidden="true" style="color: #fe5722"></i> +1 866 733 4700</h5>
+
+        <h5><i class="fa fa-envelope" aria-hidden="true" style="color: #fe5722"></i> <a href="#" target="_blank">  info@simanor.com</a> &amp; <a href="#" target="_blank"> dallas@simanor.com</a></h5>
+        
+      </div>
+    </div>
+
+     <div class="col-sm-4">
+      <div>
+      <h1 class="about_heading3">INDIA</h1>
+      <div class="seprator_1"></div>
+            <h4 style="margin-top: 0">Hyderabad</h4>
+      <address style="color: #000;padding-left: 0"><i class="fa fa-map-signs" aria-hidden="true" style="color: #fe5722"></i> #100/1, Ravi Chambers
+Kavuri Hills
+Madhapur 
+Hyderabad - 500033</address>
+
+        <h5 style="line-height: 20px"><i class="fa fa-envelope" aria-hidden="true" style="color: #fe5722"></i> <a href="#" target="_top"> hyderabad@simanor.com </a> &amp; <a href="#" target="_top"> bangalore@simanor.com</a> &amp; <a href="#" target="_top">  chennai@simanor.com</a></h5>
+        
+      </div>
+    </div>
+    
+    <div class="col-sm-4 well well-md" style="margin-bottom: 0;padding-bottom: 0">
+      <div class="row">
+     <p><span class = "error"></span></p>
+         <?php
+$nameErr = $name2Err = $emailErr = $numberErr =$messageErr ="";
+$name =$name2 = $email = $number = $message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "First Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+   if (empty($_POST["name2"])) {
+    $name2Err = "Last Name is required";
+  } else {
+    $name2 = test_input($_POST["name2"]);
+
+    if (!preg_match("/^[a-zA-Z ]*$/",$name2)) {
+      $name2Err = "Only letters and white space allowed"; 
+    }
+  }
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+   
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["number"])) {
+    $number = "number is required";
+  } else {
+    $number = test_input($_POST["number"]);
+ 
+   
+  }
+
+
+  if (empty($_POST["message"])) {
+    $messageErr = "Please provide your Comments";
+  } else {
+    $message = test_input($_POST["message"]);
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$message)) {
+      $messageErr = " Please provide Valid Comments"; 
+  }
+}
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+  if (isset($_POST['Submit'])) {
+    
+         $to = "xyz@somedomain.com";
+         $subject = "This is subject";
+         
+          $message = " First Name: ".$name."\n";
+    $message = "Last Name: ".$name2."\n";
+    $message = "Email: ".$email."\n";
+    $message = "Telephone: ".$number."\n";
+    $message = "message: ".$message."\n";
+         
+         $headers = "From:abc@somedomain.com \r\n";
+         $headers = "Cc:afgh@somedomain.com \r\n";
+         $headers = "MIME-Version: 1.0\r\n";
+         $headers = "Content-type: text/html\r\n";
+         
+         if (mail($to, $subject, $message, $headers)) 
+            {
+           
+              echo "Thank you for Contacting Us.";
+            }
+            else {
+            
+              echo "Please Try Again Later";
+            }
+           
+     }
+     
+?>
+    
+    <form name="contact-form"  method ="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+       <div class="row">
+       
+                </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="texFname" name="name" placeholder="First Name:" type="text" required="please fill"><span class="error"><?php echo $nameErr;?></span>
+        </div>
+         <div class="col-sm-6 form-group">
+          <input class="form-control" id="texLname" name="name2" placeholder="Last Name:" type="text" required=""><span class="error"><?php echo $name2Err;?></span>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="emaEmail" name="email" placeholder="Email:" type="email" required=""><span class="error"><?php echo $emailErr;?></span>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="telNumber" name="number" placeholder="Phone Number:" type="tel" pattern="[0-9]{10}" maxlength="10" required=""><span class="error"><?php echo $numberErr;?></span>
+        </div>
+        <div class="col-sm-12 form-group">
+          <textarea class="form-control" id="texComment1" name="message" placeholder="Comment:" rows="4" required=""></textarea><span class="error"><?php echo $messageErr;?></span>
+        </div>
+        <div class="row">
+        
+          <div class="col-sm-12 form-group">
+           <input type="submit" class="submit" name="Submit" value="Submit" class="btn btn-info btn2 pull-right" style="width: 150px ;margin-left: 200px;color: white ;background-color:#052e70;height: 40px">
+          </div>
+        </form></div>
+        
+    
+      </div>
+   
+    </div>
+  
+
+    </div>
+
+    <br>
+    <div class="container-fluid footer_upper">
+      <div class="col-sm-10 col-sm-offset-1">
+        <div class="col-sm-5">
+          <p class="info_span4">Call us now <br>for free consultation : <span>+1 646 340 2040</span></p>
+        </div>
+        <div class="col-sm-2">
+          <span class="info_or">OR</span>
+        </div>
+        <div class="col-sm-5">
+          <p class="info_span5">Mail Now <br>for free consultation : <span>+1 646 340 2040</span></p>
+        </div>
+      </div>
+    </div>
+
+
+  
+
+    <div class="footer_policy">
+      <a id="back-to-top" href="#" class="btn btn-default back-to-top" role="button"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+      <p class="footer_para_bottom">&copy; Copyright Simanor LLC. All Right Reserved. Designed By Glansa.com. <span class="pull-right"><a href="privacy-policy.html" class="footer_linkss">Privacy Policy</a> | <a href="term-condition.html" class="footer_linkss">Terms & Condition</a> | <a href="disclaimer.html" class="footer_linkss">Desclaimer</a></span></p>
+    </div>
+    <script>
+      function openNav() {
+          document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+          document.getElementById("mySidenav").style.width = "0";
+      }
+    </script>
+    <!-- jQuery -->
+    <script src="assets/js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
